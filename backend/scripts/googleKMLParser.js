1f093c9407cabe08fac1aec00af04c17408233be
Baynov Ivan <Jhon0@yandex.ru>
2020-07-07 23:23:26 +0500

    added ability to fill db with parsed data

--- backend/scripts/googleKMLParser.js
+++ backend/scripts/googleKMLParser.js
@@ -1,32 +1,54 @@
 const fs = require('fs');
 const convert = require('xml-js');
-const parseKML = require('parse-kml');
+const axios = require('axios');
+const db = require('../models');
 
-(() => {
+const {
+  ArtPlace, Image
+} = db;
+
+(async () => {
   const xml = fs.readFileSync('example2019.kml');
   const json = convert.xml2json(xml, { compact: true, spaces: 4 });
   const { kml } = JSON.parse(json);
   const {
     name: mapName, description: mapDescription, Folder
   } = kml.Document;
-  console.log('mapName', mapName);
-  console.log('mapDescription', mapDescription);
   // console.log('Folder', Folder);
   for (const { name: folderName, Placemark: placemarks } of Folder) {
-    console.log('folderName', folderName);
     for (const {
       name: placemarkName, description: placemarkDescription, ExtendedData: data, Point: point
     } of placemarks) {
-      const coordinates = point.coordinates._text.replace(/\s+|\n/g, '');
+      const [lat, lng] = point.coordinates._text.replace(/\s+|\n/g, '').split(',');
       const clearPlacemarkName = placemarkName._text ? placemarkName._text.replace(/\s+|\n/g, ' ')
         : placemarkName._cdata.replace(/\s+|\n/g, ' ');
       const clearPlacemarkDescription = placemarkDescription._cdata.replace(/<[^>]*>/g, '');
       const images = data.Data.value._cdata.split(' ');
+      const [artPlace, artPlaceCreated] = await ArtPlace.findOrCreate({
+        where: {
+          name: clearPlacemarkName,
+        },
+        defaults: {
+          coordinates: [lat, lng],
+          name: clearPlacemarkName,
+          description: clearPlacemarkDescription,
+          // author_name: ,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      });
+      for (const image of images) {
+        const response = await axios
+          .get(image, {
+            responseType: 'arraybuffer'
+          });
+        const buffer = Buffer.from(response.data, 'base64');
+        await Image.create({
+          data: buffer,
+          artPlace_id: artPlace.id,
+        });
+      }
       console.log('clearPlacemarkName', clearPlacemarkName);
-      console.log('clearPlacemarkDescription', clearPlacemarkDescription);
-      console.log('images', images);
-      console.log('coordinates', coordinates);
     }
   }
-  const { statistic, indicators } = JSON.parse(json);
 })();
