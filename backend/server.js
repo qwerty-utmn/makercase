29db799687c20b5bcbd1de0ff6437424eea7f97a
pokemongo22832213371488 <50990366+pokemongo22832213371488@users.noreply.github.com>
2020-07-07 13:56:03 +0500

    насрано

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const models = require('./models');
+const models = require('./models');
 
-// models.sequelize.sync().then(() => console.log('Database is fine')).catch((err) => console.log(err, 'Database is NOT fine'));
+models.sequelize.sync().then(() => console.log('Database is fine')).catch((err) => console.log(err, 'Database is NOT fine'));
 
 const appRouter = require('./routes/index');
+
 
 app.use('/api', appRouter);
 
