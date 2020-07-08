195045cc361a252fdb9f954e7dae5daa984529e3
Александр Рышков <alexryshkov72@gmail.com>
2020-07-08 17:00:42 +0500

    trying to add marker

--- client/src/components/artMap/ArtMap.jsx
+++ client/src/components/artMap/ArtMap.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import axios from 'axios';
 
-export default function ArtMap({mapOptions}) {
+const Marker = ({ text }) => <div>{text}</div>;
+
+export default function ArtMap({ mapOptions }) {
   const { center, zoom } = mapOptions;
-  return (<div style={{ width: '100%', height: 'calc(100vh - 64px)' }}>
-    <GoogleMapReact
-      bootstrapURLKeys={{ key: 'AIzaSyCJTm8QajP4RjJCtFmYeReQDfuKJXIPiO0' }}
-      defaultCenter={center}
-      defaultZoom={zoom} />
-  </div>);
+  const [artObjects, setArtObjects] = useState([]);
+  useEffect(() => {
+    async function getArtObjects() {
+      const res = await axios.get('http://localhost:3000/artPlaces');
+      setArtObjects(res.data);
+    };
+    getArtObjects();
+  }, []);
+  return (
+    <div style={{ width: '100%', height: 'calc(100vh - 64px)' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCJTm8QajP4RjJCtFmYeReQDfuKJXIPiO0' }}
+        defaultCenter={center}
+        defaultZoom={zoom}>
+        {/* {artObjects.map((artObject, markerKey) => <Marker key={markerKey} lat={artObject.coordinates[0]} lng={artObject.coordinates[1]} text={"123"} />)} */}
+      </GoogleMapReact>
+    </div>);
 }
