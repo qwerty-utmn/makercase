21b407d9f9a7338a6be37d0261ff4df7e8727688
caranell <lyachek.anton@gmail.com>
2020-07-10 01:56:22 +0500

    Added registration & login routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const db = require('../models');
 
 const { Op } = db.Sequelize;
+const { verifyToken, generateToken } = require('../services/auth');
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', verifyToken, async (req, res) => {
   try {
     const { id } = req.params;
     const user = await db.User.findOne({
@@ -51,6 +52,54 @@ router.put('/:id', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
+    const { email, password } = req.body;
+    const user = await db.User.findOne({
+      where: {
+        email: {
+          [Op.eq]: email.trim()
+        },
+        password: {
+          [Op.eq]: password.trim()
+        }
+      }
+    });
+    if (user) {
+      const token = await generateToken(user);
+      return res.json({
+        token,
+        user
+      });
+    }
+    res.status(400).json({
+      error: 1
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    let user = await db.User.findOne({
+      where: {
+        email: {
+          [Op.eq]: email.trim()
+        }
+      },
+    });
+    if (user) {
+      return res.status(400).json({
+        error: 1
+      });
+    }
+    user = await db.User.create({
+      email,
+      password,
+      name
+    });
+    const token = await generateToken(user);
+    res.status(200).json({ token, user });
   } catch (err) {
     console.error(err);
   }
