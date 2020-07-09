56902d7accb12fe7c0d6268b7579763750b04118
Caranell <lyachek.anton@gmail.com>
2020-07-10 00:27:57 +0500

    Installed jwt lib and created auth.service

---
+++ backend/services/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'SECRET';
+
+const generateToken = async (user) => {
+  const token = await jwt.sign({
+    id: user.id,
+    email: user.email
+  },
+  JWT_SECRET);
+  return token;
+};
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.get('Authorization');
+    await jwt.verify(token, JWT_SECRET, (err, res) => {
+      if (res?.id) {
+        req.user = res;
+        next();
+      } else {
+        throw new Error('bad token');
+      }
+    });
+  } catch (err) {
+    console.log('err', err);
+    return {
+      error: true
+    };
+  }
+};
